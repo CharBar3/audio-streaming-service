ecaffabfeba711df98b834173d77d11b7ef5636e
Charles Bartlett <ownageunit@gmail.com>
2022-10-12 15:12:28 -0400

    WIP fixed how icons are imported to remove console erros

--- src/components/LeftNav/LeftNav.js
+++ src/components/LeftNav/LeftNav.js
@@ -1,93 +1,42 @@
 import "./LeftNav.css";
 import NavMenu from "./NavMenu";
 
-const LeftNav = () => {
+import { ReactComponent as MusicIcon } from "../Icons/music.svg";
+import { ReactComponent as UserIcon } from "../Icons/user.svg";
+import { ReactComponent as SpeakerIcon } from "../Icons/speaker.svg";
+import { ReactComponent as PlayCircleIcon } from "../Icons/play-circle.svg";
+import { ReactComponent as ShoppingBagIcon } from "../Icons/shopping-bag.svg";
+import { ReactComponent as RadioIcon } from "../Icons/radio.svg";
+import { ReactComponent as HeartIcon } from "../Icons/heart.svg";
+import { ReactComponent as SearchIcon } from "../Icons/search.svg";
+
+const LeftNav = ({ mobileNavMenuState }) => {
+  const handleResize = (e) => {
+    console.log(e);
+  };
+
+  // document.addEventListener("resize", (e) => handleResize(e));
+
   const library = [
     {
       name: "Playlists",
       href: "place holder",
-      svg: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="feather feather-music"
-        >
-          <path d="M9 18V5l12-2v13"></path>
-          <circle cx="6" cy="18" r="3"></circle>
-          <circle cx="18" cy="16" r="3"></circle>
-        </svg>
-      ),
+      svg: <MusicIcon />,
     },
     {
       name: "Artists",
       href: "place holder",
-      svg: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="feather feather-user"
-        >
-          <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-          <circle cx="12" cy="7" r="4"></circle>
-        </svg>
-      ),
+      svg: <UserIcon />,
     },
     {
       name: "Albums",
       href: "place holder",
-      svg: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="feather feather-speaker"
-        >
-          <rect x="4" y="2" width="16" height="20" rx="2" ry="2"></rect>
-          <circle cx="12" cy="14" r="4"></circle>
-          <line x1="12" y1="6" x2="12.01" y2="6"></line>
-        </svg>
-      ),
+      svg: <SpeakerIcon />,
     },
     {
       name: "Songs",
       href: "place holder",
-      svg: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="feather feather-play-circle"
-        >
-          <circle cx="12" cy="12" r="10"></circle>
-          <polygon points="10 8 16 12 10 16 10 8"></polygon>
-        </svg>
-      ),
+      svg: <PlayCircleIcon />,
     },
   ];
 
@@ -95,86 +44,22 @@ const LeftNav = () => {
     {
       name: "Store",
       href: "place holder",
-      svg: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="feather feather-shopping-bag"
-        >
-          <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path>
-          <line x1="3" y1="6" x2="21" y2="6"></line>
-          <path d="M16 10a4 4 0 0 1-8 0"></path>
-        </svg>
-      ),
+      svg: <ShoppingBagIcon />,
     },
     {
       name: "Radio",
       href: "place holder",
-      svg: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="feather feather-radio"
-        >
-          <circle cx="12" cy="12" r="2"></circle>
-          <path d="M16.24 7.76a6 6 0 0 1 0 8.49m-8.48-.01a6 6 0 0 1 0-8.49m11.31-2.82a10 10 0 0 1 0 14.14m-14.14 0a10 10 0 0 1 0-14.14"></path>
-        </svg>
-      ),
+      svg: <RadioIcon />,
     },
     {
       name: "For You",
       href: "place holder",
-      svg: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="feather feather-heart"
-        >
-          <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-        </svg>
-      ),
+      svg: <HeartIcon />,
     },
     {
       name: "Browse",
       href: "place holder",
-      svg: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="feather feather-search"
-        >
-          <circle cx="11" cy="11" r="8"></circle>
-          <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-        </svg>
-      ),
+      svg: <SearchIcon />,
     },
   ];
   return (
--- src/components/LeftNav/MobileOpenNavButton.js
+++ src/components/LeftNav/MobileOpenNavButton.js
@@ -1,10 +1,11 @@
 import CircleButton from "../CircleButton/CircleButton";
 import "./MobileOpenNavButton.css";
+import { ReactComponent as MenuIcon } from "../Icons/menu.svg";
 
 const MobileOpenNavButton = () => {
   return (
     <div className="mobile-open-nav">
-      <CircleButton />
+      <CircleButton icon={<MenuIcon />} />
     </div>
   );
 };
